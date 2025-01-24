cef9ab07a4269b5f164fca7c99ade1b0b76a533d
rennegomes <renne.lace082@gmail.com>
2025-01-24 16:41:46 -0300

    chamariz

---
+++ app/components/chamariz.tsx
@@ -0,0 +1,24 @@
+export function Chamariz (){
+    return(
+        <div>
+            <div className="flex gap-5 justify-between flex-col items-center h-full px-5 lg:w-1/2 lg:px-0 lg:m-auto lg:gap-40 lg:flex-row">
+                <div className="font-bold my-10 md:my-0">
+                    <h1 className="text-5xl lg:text-[3vw] md:text-6xl">
+                        Proposta de
+                    </h1>
+                    <h1 className="text-6xl lg:text-[4vw] md:text-7xl mt-[-10px]">
+                        orçamento
+                    </h1>
+                    <div className="flex justify-center">
+                        <p className="text-2xl text-center bg-[#C38BBD] rounded-full mt-5 w-56 self-center lg:text-[1vw] lg:w-[10vw]">
+                            Identidade visual
+                        </p>
+                    </div>
+                </div>
+                <div className="my-5">
+                    <img src="/Vetores1.png" alt="Imagem chamativa" className="max-h-[500px]"/>
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/cabecalho";
+import { Chamariz } from "./components/chamariz";
 
 export default function Home() {
   return (
     <div className="">
       <Header />
+      <Chamariz />
     </div>
   )
 }
