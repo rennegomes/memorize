3fdac3c50b56adbce8fc565229f47eff637386b1
rennegomes <renne.lace082@gmail.com>
2025-02-11 17:16:11 -0300

    Updated: responsividade beneficios

--- app/components/cabecalho.tsx
+++ app/components/cabecalho.tsx
@@ -1,4 +1,8 @@
-export function Header(){
+interface HeaderProps {
+    clicouContato: () => void;
+};
+
+export function Header({ clicouContato }: HeaderProps){
        
     return(
         <div className="bg-[#DDE92A] h-20 flex justify-center lg:fixed lg:inset-0">
@@ -8,11 +12,15 @@ export function Header(){
                 </div>
 
                 <div className="flex gap-10 items-center font-medium max-md:text-sm max-sm:hidden">
-                    <a className="cursor-pointer">Apresentação</a>
-                    <a className="cursor-pointer">Projetos</a>
-                    <a className="cursor-pointer">Agendamento</a>
-                    <a className="cursor-pointer">Contato</a>
-                    <button className="bg-[#E74915] px-5 py-3 rounded-full border border-white text-white font-bold">Contato</button>
+                    <a className="cursor-pointer" >Apresentação</a>
+                    <a className="cursor-pointer" >Projetos</a>
+                    <a className="cursor-pointer" >Agendamento</a>
+                    <button
+                        onClick={clicouContato}
+                        className="bg-[#E74915] px-5 py-3 rounded-full border border-white text-white font-bold"
+                    >
+                        Contato
+                    </button>
                 </div>
             </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useRef } from "react";
 import { Apresentacao } from "./components/apresentacao";
 import { Beneficios } from "./components/beneficios";
 import { Header } from "./components/cabecalho";
@@ -8,16 +11,22 @@ import { Rodape } from "./components/rodape";
 import { Saudacoes } from "./components/saudacoes";
 
 export default function Home() {
+  const contatoRef = useRef<HTMLDivElement>(null);
+  const irParaContato = () => {
+    contatoRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
   return (
     <div className="">
-      <Header />
+      <Header clicouContato={irParaContato}/>
       <Chamariz />
       <Saudacoes />
       <Apresentacao />
       <Beneficios />
       <Projetos titulo="Projetos" subtitulo="Identidade visual" primeiraFoto="/identidade1.png" segundaFoto="/identidade2.png" />
       <Projetos titulo="Projetos" subtitulo="Midias sociais" primeiraFoto="/social1.jpg" segundaFoto="/social2.jpg" />
-      <Contato />
+      <div ref={contatoRef}>
+        <Contato />
+      </div>
       <Rodape />
     </div>
   )
